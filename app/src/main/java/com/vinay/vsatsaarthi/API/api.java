d1c6bee0d689fda4d443316a58bee2596c22fc99
vinay kumar <vk1230460@gmail.com>
2025-06-02 23:40:07 +0530

    commit 15

--- app/src/main/java/com/vinay/vsatsaarthi/API/api.java
+++ app/src/main/java/com/vinay/vsatsaarthi/API/api.java
@@ -1,17 +1,14 @@
 package com.vinay.vsatsaarthi.API;
 
 import android.content.Context;
-import android.util.Log;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import  com.vinay.vsatsaarthi.utils.res;
-import org.json.JSONArray;
 import org.json.JSONObject;
 public class api {
     public static final String url="https://satellite-detail.onrender.com/satellite";
