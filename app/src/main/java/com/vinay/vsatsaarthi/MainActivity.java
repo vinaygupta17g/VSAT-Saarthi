75ee69f25308bdaf7896fb461a22fd75e7e14e67
vinay kumar <vk1230460@gmail.com>
2025-06-05 16:10:48 +0530

    commit 15

--- app/src/main/java/com/vinay/vsatsaarthi/MainActivity.java
+++ app/src/main/java/com/vinay/vsatsaarthi/MainActivity.java
@@ -1,17 +1,13 @@
 package com.vinay.vsatsaarthi;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-
 import android.Manifest;
 import android.animation.ObjectAnimator;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,27 +16,25 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
-import androidx.core.widget.ImageViewCompat;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.vinay.vsatsaarthi.Models.SatelliteModel;
 import com.vinay.vsatsaarthi.databinding.ActivityMainBinding;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    FirebaseDatabase database;
     ArrayList<String> satname=new ArrayList<>();
+    ArrayList<String> dishname=new ArrayList<>();
     Location location;
-    ImageView star, world;
+    ImageView world;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         Toolbar toolbar =findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        database=FirebaseDatabase.getInstance();
         world = findViewById(R.id.earth);
         float screenWidth =getResources().getDisplayMetrics().widthPixels;
         ObjectAnimator objectAnimator2 = ObjectAnimator.ofFloat(world, "translationX", 1,-screenWidth );
@@ -77,50 +70,76 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         satname.add("Select Satellite ----");
-        database.getReference().child("Satellite").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot snapshot1:snapshot.getChildren())
-                {
-                    SatelliteModel model=snapshot1.getValue(SatelliteModel.class);
-                    model.setId(snapshot1.getKey());
-                    satname.add(model.getSatelliteName());
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,R.layout.spinner_item,satname);
+        binding.spinner.setAdapter(adapter);
 
+        //API IMPLEMENTATION
+        RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
+        JsonArrayRequest jsonArrayRequest =new JsonArrayRequest(Request.Method.GET, "https://satellite-detail.onrender.com/satellite/getsatellite", null, response -> {
+            try {
+                satname.clear();
+                for (int i=0;i<response.length();i++)
+                {
+                    JSONObject jsonObject=response.getJSONObject(i);
+                    String name=jsonObject.getString("satname");
+                    satname.add(name);
+                    adapter.notifyDataSetChanged();
+                }
+            } catch (JSONException e) {
+                Log.e("Error message",e.getMessage()+"");
             }
-        });
+        }, error -> Log.d("Error message",error.getMessage()+""));
+        requestQueue.add(jsonArrayRequest);
+
         binding.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 final String selectedItem=parent.getItemAtPosition(position).toString();
-            database.getReference().child("Satellite").addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    for(DataSnapshot snapshot1:snapshot.getChildren())
-                    {
-                        SatelliteModel model=snapshot1.getValue(SatelliteModel.class);
-                        if(model.getSatelliteName().equals(selectedItem))
+                RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
+                JsonArrayRequest jsonArrayRequest =new JsonArrayRequest(Request.Method.GET, "https://satellite-detail.onrender.com/satellite/getsatellite", null, response -> {
+                    try {
+                        for (int i=0;i<response.length();i++)
                         {
-                            binding.satLongitude.setText(model.getLongitude());
-                            binding.satlatitude.setText(model.getLatetude());
-                            binding.sataltitude.setText(model.getAltitude());
+                            JSONObject jsonObject=response.getJSONObject(i);
+                            String name=jsonObject.getString("satname");
+                            String latitude=jsonObject.getString("satlatitude");
+                            String longitude=jsonObject.getString("satlongitude");
+                            String altitude=jsonObject.getString("sataltitude");
+                            if(selectedItem.equals(name))
+                            {
+                                binding.satlatitude.setText(latitude);
+                                binding.satLongitude.setText(longitude);
+                                binding.sataltitude.setText(altitude);
+                            }
                         }
+                    } catch (JSONException e) {
+                        Log.e("Error message",e.getMessage()+"");
                     }
-                }
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                }
-            });
+                }, error -> Log.d("Error message",error.getMessage()+""));
+                requestQueue.add(jsonArrayRequest);
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
-        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,R.layout.spinner_item,satname);
-        binding.spinner.setAdapter(adapter);
+
+        dishname.add("Select Service Provider ----");
+        dishname.add("Dish TV");
+        dishname.add("DD Free Dish");
+        dishname.add("TATA Play");
+        ArrayAdapter<String> satdishadapter =new ArrayAdapter<>(this, R.layout.spinner_item,dishname);
+        binding.satdish.setAdapter(satdishadapter);
+        binding.satdish.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                binding.spinner.setSelection(adapter.getPosition(satName(parent.getItemAtPosition(position).toString())));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         binding.calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -165,8 +184,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-//        ((ImageView)findViewById(R.id.logout)).setImageDrawable(getDrawable(R.drawable.logos));
-//        ImageViewCompat.setImageTintList(((ImageView)findViewById(R.id.logout)), ColorStateList.valueOf(Color.WHITE));
     }
     public void CalculateLookUpAngle(Double latitude ,Double longitude,Double satlat,Double SatLong,Double sataltitude)
     {
@@ -225,5 +242,14 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
         return true;
+    }
+    public String satName(String serviceprovidername)
+    {
+        String satName="";
+        if(serviceprovidername.equals("Dish TV")||serviceprovidername.equals("DD Free Dish"))
+            return satName="GSAT-15";
+        else if (serviceprovidername.equals("TATA Play"))
+            return  satName="GSAT-24";
+        return satName;
     }
 }
