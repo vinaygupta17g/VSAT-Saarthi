4e6f20aa99e8475901b322f6a09962c00ee1293e
vinay kumar <vk1230460@gmail.com>
2025-06-12 19:43:16 +0530

    commit 16

--- app/src/main/java/com/vinay/vsatsaarthi/LossCalculator.java
+++ app/src/main/java/com/vinay/vsatsaarthi/LossCalculator.java
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.vinay.vsatsaarthi.databinding.FragmentLossCalculatorBinding;
+
 public class LossCalculator extends Fragment {
+    FragmentLossCalculatorBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        
-        return inflater.inflate(R.layout.fragment_loss_calculator, container, false);
+
+        binding =FragmentLossCalculatorBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
