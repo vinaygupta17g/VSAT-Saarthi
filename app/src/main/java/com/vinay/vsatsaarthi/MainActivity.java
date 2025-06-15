03ba9665843eb923c4dc4201dde4385ebf97e403
vinay kumar <vk1230460@gmail.com>
2025-06-15 11:19:29 +0530

    commit 17

--- app/src/main/java/com/vinay/vsatsaarthi/MainActivity.java
+++ app/src/main/java/com/vinay/vsatsaarthi/MainActivity.java
@@ -1,8 +1,14 @@
 package com.vinay.vsatsaarthi;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.vinay.vsatsaarthi.Adapters.FragmentAdapter;
 import com.vinay.vsatsaarthi.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +24,18 @@ public class MainActivity extends AppCompatActivity {
         FragmentAdapter adapter =new FragmentAdapter(getSupportFragmentManager());
         binding.viewPager.setAdapter(adapter);
         binding.tabLayout.setupWithViewPager(binding.viewPager);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent =new Intent(MainActivity.this,SignInActivity.class);
+        startActivity(intent);
+        finish();
+        return true;
     }
 }
