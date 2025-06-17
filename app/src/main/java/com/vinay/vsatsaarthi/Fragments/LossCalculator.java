997617d8cdc358d3d57e7cf61f9ff53ebaccd0c0
vinay kumar <vk1230460@gmail.com>
2025-06-17 12:01:23 +0530

    17-06-2025

--- app/src/main/java/com/vinay/vsatsaarthi/Fragments/LossCalculator.java
+++ app/src/main/java/com/vinay/vsatsaarthi/Fragments/LossCalculator.java
@@ -4,21 +4,16 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.vinay.vsatsaarthi.databinding.FragmentLossCalculatorBinding;
-
 import org.json.JSONObject;
-
 public class LossCalculator extends Fragment {
     String gain="";
     FragmentLossCalculatorBinding binding;
