9f43fafa1b64dc9349f7e0d9c1c2f4b8d28db88f
vinay kumar <vk1230460@gmail.com>
2025-06-17 11:26:50 +0530

    17-06-2025

--- app/src/main/java/com/vinay/vsatsaarthi/Fragments/LossCalculator.java
+++ app/src/main/java/com/vinay/vsatsaarthi/Fragments/LossCalculator.java
@@ -2,14 +2,11 @@ package com.vinay.vsatsaarthi.Fragments;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
 import com.vinay.vsatsaarthi.databinding.FragmentLossCalculatorBinding;
 public class LossCalculator extends Fragment {
     FragmentLossCalculatorBinding binding;
